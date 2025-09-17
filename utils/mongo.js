9fef221d3cdbacaf5b4f8b636c77ba1ad8306271
Patryk Stolinski <patrykstolinski@gmail.com>
2025-09-17 13:01:54 +0200

    Added helped utils for transforming mongo data. Refactored get /resources endpoint.

---
+++ utils/mongo.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const { Types } = mongoose;
+
+export function toObjectId(id) {
+    if (!Types.ObjectId.isValid) {
+        const err = new Error("Ungueltige ObjectId");
+        err.status = 400;
+        throw err;
+    }
+    if (typeof id !== "string") {
+        const err = new Error("ObjectId muss ein String sein!");
+        err.status = 400;
+        throw err
+    }
+    return new Types.ObjectId(id);
+};
+
+export function toClient(doc) {
+    if (!doc) return doc;
+    const { _id, __v, ...rest } = doc;
+    return {id: String(_id), ...rest};
+}
